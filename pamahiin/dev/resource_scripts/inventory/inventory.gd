bd690a89f19c2d19e744c8b1796839f56e545403
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2025-11-10 21:58:27 +0800

    Re-implement inventory base elements
    Inventory has been updated, but 1) no defined global (?) source for the
    actual held items has been set yet, and 2) existing item resources have
    not been updated.

--- pamahiin/dev/resource_scripts/inventory/inventory.gd
+++ pamahiin/dev/resource_scripts/inventory/inventory.gd
@@ -1,16 +1,45 @@
 class_name Inventory
 extends Resource
 
-signal update
+signal updated
 
-@export var slots: Array[InvSlot]
+@export var slots: Array[InvSlot] = []
 
-func insert(item: InvItem):
-
-	var emptyslots = slots.filter(func(slot): return slot.item==null)
-	if !emptyslots.is_empty():
-		emptyslots[0].item = item
-		
-		
-	update.emit()
+func lose_slot(idx: int, count: int = 1) -> void:
+	assert(count > 0, "Cannot lose an item < 1 times.")
+	assert(idx >= 0 and idx < slots.size(), "Index of slot to lose invalid.")
 	
+	slots[idx].count -= count
+	
+	if slots[idx].count <= 0:
+		slots.remove_at(idx)
+	
+	updated.emit()
+
+func lose_item(item: InvItem, count: int = 1) -> void:
+	assert(count > 0, "Cannot lose an item < 1 times.")
+	
+	var existing_slot_idx := slots.find_custom(func (s): return s.item == item)
+	
+	if existing_slot_idx == -1:
+		push_error("Attempted to lose nonexistent item.")
+		return
+	
+	lose_slot(existing_slot_idx, count)
+
+func obtain(item: InvItem, count: int = 1) -> void:
+	assert(count > 0, "Cannot obtain an item < 1 times.")
+	
+	var existing_slot_idx := slots.find_custom(func (s): return s.item == item)
+
+	if existing_slot_idx == -1:
+		var new_slot := InvSlot.new()
+		new_slot.item = item
+		new_slot.count = count
+		
+		slots.append(new_slot)
+	else:
+		var old_slot = slots[existing_slot_idx]
+		old_slot.count += count
+
+	updated.emit()
